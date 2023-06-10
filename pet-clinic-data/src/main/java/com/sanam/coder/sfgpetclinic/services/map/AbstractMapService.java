f5c294eac6109ffd243f16e1b54f0be40937cccc
SamratCoder710 <shubhamksrathod26@gmail.com>
2023-06-10 10:29:19 -0400

    List of Owners and Vets and Refactoring to generate automatic Ids

--- pet-clinic-data/src/main/java/com/sanam/coder/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/sanam/coder/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,20 +1,32 @@
 package com.sanam.coder.sfgpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
+
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
-public abstract class AbstractMapService<T,ID> {
+import com.sanam.coder.sfgpetclinic.model.BaseEntity;
+
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
 	
-	protected Map<ID,T> map = new HashMap<>();
+	protected Map<Long,T> map = new HashMap<>();
 	
 	Set<T> findAll(){
 		return new HashSet<>(map.values());
 	}
 	
-	T save(ID id,T object) {
-		map.put(id, object);
+	T save(T object) {
+		if(object != null) {
+			if(object.getId() == null) {
+				object.setId(getNextId());
+			}
+			map.put(object.getId(), object);
+		}else {
+			throw new RuntimeException("Object cannot be null");
+		}
 		return object;
 	}
 	
@@ -28,12 +40,24 @@ public abstract class AbstractMapService<T,ID> {
 	}
 	
 	void delete(T object) {
-		for(ID m:map.keySet()) {
+		for(Long m:map.keySet()) {
 			if(map.get(m).equals(object)) {
 				map.remove(m);
 			}
 		}
 	}
+	
+	private Long getNextId() {
+		Long nextId = null;
+		try {
+			nextId =  Collections.max(map.keySet())+1;
+		}catch(NoSuchElementException e) {
+			nextId = 1L;
+		}
+		return nextId;
+		
+	}
+	
 	
 	
 	
