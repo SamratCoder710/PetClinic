8be2ba5cf6024a38071f936fbe883e8c6f950ce3
Shubham Rathod <shubhamksrathod26@gmail.com>
2023-06-12 15:11:36 -0400

    added specialities , added to vets

---
+++ pet-clinic-data/src/main/java/com/sanam/coder/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.sanam.coder.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity{
+
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
