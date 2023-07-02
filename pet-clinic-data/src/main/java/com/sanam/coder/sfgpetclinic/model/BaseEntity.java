2870684b5bd40d0d42f387be0ee02040983de64e
SamratCoder710 <shubhamksrathod26@gmail.com>
2023-07-02 16:50:50 +0530

    Added basenetity with JPA annotation

--- pet-clinic-data/src/main/java/com/sanam/coder/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/sanam/coder/sfgpetclinic/model/BaseEntity.java
@@ -2,9 +2,17 @@ package com.sanam.coder.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	private String name;
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
